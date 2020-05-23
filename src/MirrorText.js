9450e85d633e743491f4d046d0caf65c4e68bd69
pacotiger <padilla.fco@gmail.com>
2020-05-23 19:10:18 +0000

    proposed solution

--- src/MirrorText.js
+++ src/MirrorText.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 
 class MirrorText extends Component {
+  state = {
+    value: "",
+  }
+  
+  updateValue = (event) => {
+    this.setState(() => ({
+      value: event,
+    }))
+  }
 
   render() {
     console.log("MirrorText iniciando... this.props ", this.props, " this.state ", this.state)
+    const { value } = this.state;
     return (
       <div className="container">
-        <input type="text" placeholder="Say Something" />
+        <input 
+          type="text" 
+          placeholder="Say Something" 
+          value={value}
+          onChange={(event) => this.updateValue(event.target.value)}/>
         <p className="echo">Echo:</p>
-        <p>This should mirror the text you typed into the input field.</p>
+        <p>{value}</p>
       </div>
     )
   }
